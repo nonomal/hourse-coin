b8702594f787fff3ab7621956097c0dd6cb8e9ca
dandelion <1993466159@qq.com>
2026-01-06 15:42:18 +0800

    feat(src): 添加新增个人信息与删除个人信息功能

--- src/entrypoints/sidepanel/App.tsx
+++ src/entrypoints/sidepanel/App.tsx
@@ -6,6 +6,7 @@ import {
 	Input,
 	Modal,
 	message,
+	Popconfirm,
 	Row,
 	Space,
 } from "antd";
@@ -19,37 +20,56 @@ interface infoInter {
 	appointmentQuantity: string;
 }
 
-const info: infoInter[] = [
-	{
-		userName: "张三",
-		idCard: "360702000000000000",
-		phone: "18300000000",
-		appointmentBranch: "江西省分行",
-		appointmentQuantity: "10",
-	},
-];
-
 const App: React.FC = () => {
+	const [info, setInfo] = useState<infoInter[]>([]);
+
+	// 获取本地存储的数据
+	const getInfo = async () => {
+		try {
+			const res = await storage.getItem<infoInter[]>("local:info");
+			if (!res) return;
+			setInfo(res);
+		} catch {
+			cusAlert("error", "获取存储数据出错");
+		}
+	};
+
+	// 页面挂载时获取一次数据
+	useEffect(() => {
+		getInfo();
+	}, []);
+
 	// 处理弹框modal
 	const [title, setTitle] = useState("标题");
 	const [open, setOpen] = useState(false);
 	const [confirmLoading, setConfirmLoading] = useState(false);
 
+	// 弹出对话框，新增与编辑事件
 	const showModal = (title: string) => {
 		setTitle(title);
 		setOpen(true);
 	};
 
-	const submit = () => {
+	// 弹框内表单处理
+	const [form] = Form.useForm();
+
+	const handleSubmit = async (value: infoInter) => {
 		setConfirmLoading(true);
-		setTimeout(() => {
-			setOpen(false);
-			setConfirmLoading(false);
-			cusAlert("success", "修改完成");
-		}, 500);
+		const info = await storage.getItem<infoInter[]>("local:info");
+		if (info) {
+			info.push(value);
+			await storage.setItem("local:info", info);
+		} else {
+			await storage.setItem("local:info", [value]);
+		}
+		getInfo();
+		setOpen(false);
+		setConfirmLoading(false);
+		cusAlert("success", "修改完成");
+		form.resetFields();
 	};
 
-	const cancel = () => {
+	const handleCancel = () => {
 		setOpen(false);
 	};
 
@@ -59,7 +79,7 @@ const App: React.FC = () => {
 		messageApi.open({ type, content: message });
 	};
 
-	// 处理填充事件
+	// 填写逻辑
 	const handleFill = (person: (typeof info)[number]) => {
 		browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
 			if (!tabs[0]?.id) {
@@ -89,6 +109,16 @@ const App: React.FC = () => {
 			);
 		});
 	};
+
+	// 删除事件
+	const handleDelete = async (idCard: string) => {
+		const res = await storage.getItem<infoInter[]>("local:info");
+		const info = res?.filter((item) => item.idCard !== idCard);
+		await storage.setItem("local:info", info);
+		getInfo();
+		cusAlert("success", "删除成功");
+	};
+
 	return (
 		<>
 			{contextHolder}
@@ -97,16 +127,16 @@ const App: React.FC = () => {
 				open={open}
 				confirmLoading={confirmLoading}
 				footer={false}
-				onOk={submit}
-				onCancel={cancel}
+				onCancel={handleCancel}
 			>
 				<Form
+					form={form}
 					name="basic"
 					labelCol={{ span: 8 }}
 					wrapperCol={{ span: 16 }}
 					style={{ maxWidth: 600 }}
 					initialValues={{ remember: true }}
-					onFinish={submit}
+					onFinish={handleSubmit}
 					onFinishFailed={() => {
 						cusAlert("error", "请输入正确完整信息");
 					}}
@@ -164,7 +194,7 @@ const App: React.FC = () => {
 
 					<Form.Item>
 						<div className="flex justify-end gap-4">
-							<Button onClick={cancel}>取消</Button>
+							<Button onClick={handleCancel}>取消</Button>
 							<Button type="primary" htmlType="submit">
 								确定
 							</Button>
@@ -180,40 +210,54 @@ const App: React.FC = () => {
 				</header>
 				<main className="flex flex-col p-4">
 					<Row gutter={[8, 8]}>
-						{info.map((person) => (
-							<Col key={person.idCard} xs={24} sm={12} md={8} lg={6} xl={6}>
-								<Card style={{ width: "100%" }}>
-									<div className="flex flex-col gap-6">
-										<div className="flex flex-col gap-2">
-											<p>姓名：{person.userName}</p>
-											<p>证件号：{person.idCard}</p>
-											<p>手机号：{person.phone}</p>
-											<p>预约网点：{person.appointmentBranch}</p>
-											<p>预约数量：{person.appointmentQuantity}</p>
+						{info.length !== 0 ? (
+							info.map((person) => (
+								<Col key={person.idCard} xs={24} sm={12} md={8} lg={6} xl={6}>
+									<Card style={{ width: "100%" }}>
+										<div className="flex flex-col gap-6">
+											<div className="flex flex-col gap-2">
+												<p>姓名：{person.userName}</p>
+												<p>证件号：{person.idCard}</p>
+												<p>手机号：{person.phone}</p>
+												<p>预约网点：{person.appointmentBranch}</p>
+												<p>预约数量：{person.appointmentQuantity}</p>
+											</div>
+											<div className="flex items-center justify-between gap-4">
+												<Button
+													color="primary"
+													variant="solid"
+													onClick={() => handleFill(person)}
+												>
+													填写
+												</Button>
+												<Button
+													color="green"
+													variant="solid"
+													onClick={() => showModal("编辑")}
+												>
+													编辑
+												</Button>
+												<Popconfirm
+													title="删除个人信息"
+													description="您确认删除此条个人信息吗"
+													cancelText="取消"
+													okText="确定"
+													onConfirm={() => handleDelete(person.idCard)}
+												>
+													<Button color="danger" variant="solid">
+														删除
+													</Button>
+												</Popconfirm>
+											</div>
 										</div>
-										<div className="flex items-center justify-between gap-4">
-											<Button
-												color="primary"
-												variant="solid"
-												onClick={() => handleFill(person)}
-											>
-												填写
-											</Button>
-											<Button
-												color="green"
-												variant="solid"
-												onClick={() => showModal("编辑")}
-											>
-												编辑
-											</Button>
-											<Button color="danger" variant="solid">
-												删除
-											</Button>
-										</div>
-									</div>
-								</Card>
-							</Col>
-						))}
+									</Card>
+								</Col>
+							))
+						) : (
+							<div className="self-center">
+								您还未添加信息，请点击右上角添加个人预约信息
+							</div>
+						)}
 					</Row>
 				</main>
 			</main>
